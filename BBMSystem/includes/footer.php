04a362a6c7ccb75f3913c2234680d30c816cf4f3
balachandar-web <76194348+balachandar-web@users.noreply.github.com>
2024-11-22 11:18:06 +0530

    Delete BBMSystem directory

--- BBMSystem/includes/footer.php
+++
@@ -1,8 +0,0 @@
-  
-  
-  <footer class="py-5 bg-inverse">
-        <div class="container">
-            <p class="m-0 text-center text-white">© 2024 made by balachandar</p>
-        </div>
-        <!-- /.container -->
-    </footer>
